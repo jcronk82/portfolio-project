1fa719b9f30f0b34b16a8a3b0862295e143e8f28
Justin Cronk <jcronk782@gmail.com>
2020-07-16 10:50:02 -0400

    Blog functionality and polish

--- blog/models.py
+++ blog/models.py
@@ -7,8 +7,11 @@ class Blog(models.Model):
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
 
-# Create a migration
+    def __str__(self):
+        return self.title
 
-# Migrate
+    def summary(self):
+        return self.body[:100]
 
-# Add to the admin
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
