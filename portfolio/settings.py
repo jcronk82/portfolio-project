c304a4186b56e8adeedbc696552ee12f0c82453c
Justin Cronk <jcronk782@gmail.com>
2020-07-16 10:59:15 -0400

    Update

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER': 'postgres',
-        'PASSWORD': 'Habk9191',
+        'PASSWORD': 'Django54321',
         'HOST': '127.0.0.1',
         'PORT': '5432',
 
